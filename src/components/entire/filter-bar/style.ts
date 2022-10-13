1272caf223f6a8f85b865ea142965acc651e2ee4
miles <a09112332@gmail.com>
2022-10-14 00:19:26 +0800

    fitler-bar

--- src/components/entire/filter-bar/style.ts
+++ src/components/entire/filter-bar/style.ts
@@ -1,5 +1,28 @@
 import styled from "styled-components"
 
-export const FilterBarWrapper = styled.div`
+import { ThemePropsType } from "@/theme"
 
+export const FilterBarWrapper = styled.div<ThemePropsType>`
+  padding: 8px 16px;
+  border-bottom: 1px solid #f2f2f2;
+
+  .filter-list {
+    display: flex;
+  }
+
+  .filter-item {
+    margin: 0 4px 0 8px;
+    padding: 6px 12px;
+    font-size: 14px;
+    color: ${props => props.theme.text.primary};
+    border: 1px solid #dce0e0;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  .active {
+    background-color: #008489;
+    border-color: #008489;
+    color: #fff;
+  }
 `
